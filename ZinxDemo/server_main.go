6578b0b9279c41f7cd2eb65c9917329e00ad8e56
Mrzhenxing <704801835@qq.com>
2019-05-23 19:57:56 +0800

    链接的封装和业务的绑定修正

---
+++ ZinxDemo/server_main.go
@@ -0,0 +1,18 @@
+/**
+ zinx v0.1 应用
+*/
+package main
+
+import (
+	"zinx/net"
+)
+
+func main() {
+	//创建一个zinx server对象
+	s := net.NewServer("zinx v0.1")
+
+	//让server对象 启动服务
+	s.Serve()
+
+	return
+}
